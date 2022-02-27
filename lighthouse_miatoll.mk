bcf62db43734ee0bec51e6967fa46ecfe7c45a95
Stealth1226 <rishawniyer06@gmail.com>
2022-02-27 18:38:47 +0530

    miatoll: update stock fingerprints to latest

--- lighthouse_miatoll.mk
+++ lighthouse_miatoll.mk
@@ -23,8 +23,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="miatoll_global-user 11 RKQ1.200826.002 V12.5.2.0.RJZMIXM release-keys"
+    PRIVATE_BUILD_DESC="miatoll_global-user 11 RKQ1.200826.002 V12.5.4.0.RJZMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/miatoll_global/miatoll:11/RKQ1.200826.002/V12.5.2.0.RJZMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/miatoll_global/miatoll:11/RKQ1.200826.002/V12.5.4.0.RJZMIXM:user/release-keys
 
 LIGHTHOUSE_BUILD_TYPE := OFFICIAL
